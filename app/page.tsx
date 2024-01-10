ca5db602e03c018ebdad2dc4f88dd3133d489ca3
Ccreature09 <gabriellikov@gmail.com>
2024-01-10 21:36:56 +0200

    style:Better homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,34 @@
 import { Navbar } from "@/components/functional/navbar";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 export default function Home() {
+  const backgroundImageStyle = {
+    backgroundImage:
+      "url('https://i.ibb.co/k2Lnz9t/blurry-gradient-haikei-1.png')",
+    backgroundSize: "cover",
+    width: "100%",
+  };
+
   return (
     <>
-      <Navbar></Navbar>
+      <div style={backgroundImageStyle} className="h-screen flex-row">
+        <Navbar></Navbar>
+        <div className="flex flex-row">
+          <div className="mx-24 mt-96 ">
+            <p className="text-xl font-semibold text-white">Welcome to</p>
+            <p className="text-9xl font-black text-white">PRJ Charlie</p>
+            <Button className="mt-4 bg-opacity-10 bg-white hover:opacity-90">
+              <p className="font-semibold"> Start your journey!</p>
+            </Button>
+          </div>
+          <div className="">
+            <img
+              src="https://img.itch.zone/aW1nLzQ5NDI5NjkuZ2lm/original/z2%2Bcie.gif"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
