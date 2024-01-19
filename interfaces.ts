a8fab3ee3dc49b5da445b18fef5e76a3ba57bbf1
Ccreature09 <gabriellikov@gmail.com>
2024-01-19 09:16:40 +0200

    fix: added more level attributes

--- interfaces.ts
+++ interfaces.ts
@@ -1,7 +1,15 @@
+import { Timestamp, FieldValue } from "firebase/firestore";
+
 export interface Level {
   id: number;
+  seed: string;
+  publishDate: Timestamp | FieldValue;
   grid: string;
   name: string;
+  author: string;
+  authorUid: string;
+  tags: string[];
+  imgURL: string;
   description: string;
   difficulty: string;
   unlimited: boolean;
