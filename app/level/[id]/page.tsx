5c27cd1ba22a2b6074f614cd6547165af8e566f9
Ccreature09 <gabriellikov@gmail.com>
2024-01-19 09:21:15 +0200

    style + feat: bg fix and unity implemented

--- app/level/[id]/page.tsx
+++ app/level/[id]/page.tsx
@@ -4,6 +4,15 @@ import { query, collection, where, getDocs } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
 import { Level } from "@/interfaces";
 import { Navbar } from "@/components/functional/navbar";
+import UnityEmbed from "@/components/functional/unity";
+
+const backgroundImageStyle = {
+  backgroundImage:
+    "url('https://i.ibb.co/k2Lnz9t/blurry-gradient-haikei-1.png')",
+  backgroundSize: "cover",
+  width: "100%",
+};
+
 export default function Page({ params }: { params: { id: string } }) {
   const [slug, setSlug] = useState(params.id);
   const [level, setlevel] = useState<Level>();
@@ -37,15 +46,27 @@ export default function Page({ params }: { params: { id: string } }) {
     fetchProductData();
   }, [slug]);
 
+  useEffect(() => {
+    level && console.log(level);
+  }, [level]);
+
   return (
     <>
-      <Navbar></Navbar>
-      <div className="m-5">
-        <div className="bg-slate-300 w-1/4 h-screen">
-          <p className="text-4xl font-bold text-center pt-10">{level?.name}</p>
-          <p className="p-5 font-medium">{level?.description}</p>
+      <div style={backgroundImageStyle} className="h-full flex-row">
+        <Navbar></Navbar>
+        <div className="m-5 gap-3 flex">
+          <div className="bg-slate-300 w-1/4 h-screen">
+            <p className="text-4xl font-bold text-center pt-10">
+              {level?.name}
+            </p>
+            <p className="p-5 font-medium">{level?.description}</p>
+          </div>
+          {level && (
+            <div className="w-3/4">
+              <UnityEmbed level={level}></UnityEmbed>
+            </div>
+          )}
         </div>
-        <div></div>
       </div>
     </>
   );
