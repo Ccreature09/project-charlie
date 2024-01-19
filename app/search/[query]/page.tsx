d346cdd49c7e22b647ff123f571905bfc2565187
Ccreature09 <gabriellikov@gmail.com>
2024-01-19 09:19:09 +0200

    style: bg image

--- app/search/[query]/page.tsx
+++ app/search/[query]/page.tsx
@@ -7,6 +7,14 @@ import { Navbar } from "@/components/functional/navbar";
 import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
 import Link from "next/link";
+
+const backgroundImageStyle = {
+  backgroundImage:
+    "url('https://i.ibb.co/k2Lnz9t/blurry-gradient-haikei-1.png')",
+  backgroundSize: "cover",
+  width: "100%",
+};
+
 export default function Page({ params }: { params: { query: string } }) {
   const [slug, setSlug] = useState(params.query);
   const [levels, setLevels] = useState<Level[]>([]);
@@ -15,6 +23,9 @@ export default function Page({ params }: { params: { query: string } }) {
     const fetchLevels = async () => {
       if (slug) {
         const searchQuery = decodeURIComponent(slug);
+        console.log(slug);
+        console.log(searchQuery);
+
         const q = query(
           collection(db, "levels"),
           where("name", ">=", searchQuery),
@@ -42,60 +53,62 @@ export default function Page({ params }: { params: { query: string } }) {
 
   return (
     <>
-      <Navbar></Navbar>
+      <div style={backgroundImageStyle} className="h-screen ">
+        <Navbar></Navbar>
 
-      <div className="m-20">
-        <p className="text-3xl mb-16">
-          Results for <span className="font-semibold">{slug}</span>:
-        </p>
+        <div className="m-20">
+          <p className="text-3xl mb-16">
+            Results for <span className="font-semibold">{slug}</span>:
+          </p>
 
-        <div className="m-10">
-          {levels.map((level) => (
-            <div key={level.id}>
-              <Link
-                href={`/level/${encodeURIComponent(level.id)}`}
-                className="flex bg-blue-100 rounded-lg p-5 my-5"
-              >
-                <img
-                  src="https://etc.usf.edu/clipart/21900/21988/square_21988_md.gif"
-                  alt={level.name + " image"}
-                  className="w-48"
-                />
-                <div className="mx-10">
-                  <div className="flex h-5 items-center space-x-4 ">
-                    <Separator orientation="vertical" />
-                    <p className="text-3xl font-semibold text-center ">
-                      {level.name}
-                    </p>
-                    <Separator orientation="vertical" className="bg-black" />
-                    <Badge className="bg-slate-400 rounded-lg">
-                      <img
-                        src="https://i.ibb.co/VJhxNJV/Icon-1.png"
-                        className="w-3 mr-2"
-                        alt=""
-                      />
-                      10x10
-                    </Badge>
+          <div className="m-10">
+            {levels.map((level) => (
+              <div key={level.id}>
+                <Link
+                  href={`/level/${encodeURIComponent(level.id)}`}
+                  className="flex bg-blue-100 rounded-lg p-5 my-5"
+                >
+                  <img
+                    src="https://etc.usf.edu/clipart/21900/21988/square_21988_md.gif"
+                    alt={level.name + " image"}
+                    className="w-48"
+                  />
+                  <div className="mx-10">
+                    <div className="flex h-5 items-center space-x-4 ">
+                      <Separator orientation="vertical" />
+                      <p className="text-3xl font-semibold text-center ">
+                        {level.name}
+                      </p>
+                      <Separator orientation="vertical" className="bg-black" />
+                      <Badge className="bg-slate-400 rounded-lg">
+                        <img
+                          src="https://i.ibb.co/VJhxNJV/Icon-1.png"
+                          className="w-3 mr-2"
+                          alt=""
+                        />
+                        10x10
+                      </Badge>
 
-                    <Badge className="bg-slate-400 rounded-lg">
-                      <img
-                        src="https://i.ibb.co/KhG75bv/Rectangle-5.png"
-                        className="w-4 mr-2"
-                        alt=""
-                      />
-                      {level.difficulty}
-                    </Badge>
-                    <Badge className="bg-slate-400 rounded-lg">
-                      {level.unlimited ? "Unlimited" : "Limited"}
-                    </Badge>
+                      <Badge className="bg-slate-400 rounded-lg">
+                        <img
+                          src="https://i.ibb.co/KhG75bv/Rectangle-5.png"
+                          className="w-4 mr-2"
+                          alt=""
+                        />
+                        {level.difficulty}
+                      </Badge>
+                      <Badge className="bg-slate-400 rounded-lg">
+                        {level.unlimited ? "Unlimited" : "Limited"}
+                      </Badge>
+                    </div>
+                    <p className="p-5 ml-4 font-medium">{level.description}</p>
                   </div>
-                  <p className="p-5 ml-4 font-medium">{level.description}</p>
-                </div>
-              </Link>
-            </div>
-          ))}
+                </Link>
+              </div>
+            ))}
+          </div>
+          <div></div>
         </div>
-        <div></div>
       </div>
     </>
   );
