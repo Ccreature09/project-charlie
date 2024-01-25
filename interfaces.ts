d3df3d261baf3b63fd3a7de4ab0cd962c4a0310d
Ccreature09 <gabriellikov@gmail.com>
2024-01-25 21:58:52 +0200

    chore: added more level props

--- interfaces.ts
+++ interfaces.ts
@@ -2,24 +2,26 @@ import { Timestamp, FieldValue } from "firebase/firestore";
 
 export interface Level {
   id: number;
+  likes: number;
   seed: string;
-  publishDate: Timestamp | FieldValue;
   grid: string;
   name: string;
   author: string;
   authorUID: string;
-  tags: string[];
+  pfp: string;
   imgURL: string;
   description: string;
   difficulty: string;
+  tags: string[];
   unlimited: boolean;
+  publishDate: Timestamp | FieldValue;
 }
 
 export interface User {
   uid: string;
   pfp: string;
   username: string;
-  dateOfRegistration: Timestamp | FieldValue;
   badges: string[];
   levels: number[];
+  dateOfRegistration: Timestamp | FieldValue;
 }
