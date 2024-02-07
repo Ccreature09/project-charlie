8dac7fe3184561bcb662aff6a47071eeaa934669
Ccreature09 <gabriellikov@gmail.com>
2024-02-07 02:17:50 +0200

    style

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -4,12 +4,17 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { User, Level } from "@/interfaces";
 import React, { useState, useEffect } from "react";
 import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import {
   getDocs,
   where,
   query,
   collection,
-  doc,
-  getDoc,
   Timestamp,
 } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
@@ -17,7 +22,6 @@ import { db, auth } from "@/firebase/firebase";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -90,7 +94,7 @@ export default function Page({ params }: { params: { id: string } }) {
         <Navbar></Navbar>
 
         {user && (
-          <div className="bg-gray-500 bg-opacity-50 relative h-[50vh] mt-12 mx-10 rounded-xl g-cover">
+          <div className="bg-gray-500 bg-opacity-50 relative h-[50vh] mt-5 mx-10 rounded-xl g-cover">
             <div className="absolute bottom-5 left-5">
               <div className="flex">
                 <Avatar className="w-32 h-32">
@@ -108,13 +112,15 @@ export default function Page({ params }: { params: { id: string } }) {
             </div>
             <div className="absolute top-5 right-5">
               {isAdmin && (
-                <Button className="bg-transparent py-8 hover:bg-white hover:bg-opacity-10 rounded-xl">
-                  <img
-                    src="https://static-00.iconduck.com/assets.00/settings-icon-2048x2046-cw28eevx.png"
-                    alt="settings"
-                    className="w-10"
-                  />
-                </Button>
+                <Link href={"/profile/settings"}>
+                  <Button className="bg-transparent py-8 hover:bg-white hover:bg-opacity-10 rounded-xl">
+                    <img
+                      src="https://static-00.iconduck.com/assets.00/settings-icon-2048x2046-cw28eevx.png"
+                      alt="settings"
+                      className="w-10"
+                    />
+                  </Button>
+                </Link>
               )}
             </div>
           </div>
@@ -122,51 +128,53 @@ export default function Page({ params }: { params: { id: string } }) {
 
         <div className="mx-10 text-4xl text-white font-bold">
           <p className="mt-10 ml-5">My Levels</p>
-          {/* Add logic to display user's levels here */}
-          <div className="gap-3">
-            {levels.map((level) => (
-              <div key={level.id} className="w-1/6 m-8">
-                <Link href={`/level/${level.id}`}>
-                  <Card>
-                    <CardHeader>
-                      <CardTitle className="text-5xl font-bold">
-                        {level.name}
-                      </CardTitle>
-                      <div className="gap-2 flex">
-                        <Badge className="bg-slate-400 mt-2 rounded-lg">
-                          <img
-                            src="https://i.ibb.co/VJhxNJV/Icon-1.png"
-                            className="w-3 mr-2"
-                            alt=""
-                          />
-                          10x10
-                        </Badge>
-                        <Badge className="bg-slate-400 mt-2 rounded-lg">
-                          <img
-                            src="https://i.ibb.co/KhG75bv/Rectangle-5.png"
-                            className="w-4 mr-2"
-                            alt=""
-                          />
-                          {level.difficulty}
-                        </Badge>
-                        <Badge className="bg-slate-400 mt-2 rounded-lg">
-                          {level.unlimited ? "Unlimited" : "Limited"}
-                        </Badge>
-                      </div>
-                      <img src="" alt="" />
-                      <CardDescription>{level.description}</CardDescription>
-                    </CardHeader>
+          {levels.length > 0 ? (
+            <Carousel className="mx-20 mt-5">
+              <CarouselContent>
+                {levels.map((level) => (
+                  <CarouselItem key={level.id} className="basis-1/4">
+                    <Card>
+                      <CardHeader>
+                        <CardTitle>{level.name}</CardTitle>
+                      </CardHeader>
+                      <CardContent>
+                        <p>Card Content</p>
+                      </CardContent>
+                      <CardFooter>
+                        <Link href={`/level/${level.id}`}>
+                          <Button>Play</Button>
+                        </Link>
+                        <div className="flex mx-4 gap-2">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="currentColor"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            className="my-auto"
+                          >
+                            <path d="M14 9V5a3 3 0 0 0-3-3l-4 9v11h11.28a2 2 0 0 0 2-1.7l1.38-9a2 2 0 0 0-2-2.3zM7 22H4a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h3"></path>
+                          </svg>
 
-                    <CardFooter className="text-xl">
-                      {level.publishDate instanceof Timestamp
-                        ? level.publishDate.toDate().toLocaleDateString()
-                        : ""}
-                    </CardFooter>
-                  </Card>
-                </Link>
-              </div>
-            ))}
-          </div>
+                          <p className="text-xl">{level.likes}</p>
+                        </div>
+                      </CardFooter>
+                    </Card>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
+          ) : (
+            <p className="text-xl m-10">No levels yet...</p>
+          )}
+
+          <div className="gap-2 flex"></div>
         </div>
       </div>
     </>
