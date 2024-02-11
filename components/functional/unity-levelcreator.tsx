91ef594835efc9caefec3413a7acfa14090bc6cb
Ccreature09 <gabriellikov@gmail.com>
2024-02-11 13:39:42 +0200

    fix: unity level creator component

--- components/functional/unity-levelcreator.tsx
+++ components/functional/unity-levelcreator.tsx
@@ -1,22 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 import { Button } from "../ui/button";
-import { Level } from "@/interfaces";
 import { Progress } from "../ui/progress";
-export default function UnityLevelEmbed({ level }: { level?: Level }) {
-  const [loadingPercentage, setLoadingPercentage] = useState(0);
+
+export default function UnityLevelEmbed({
+  onGameStatusChange,
+  onScreenshot,
+  onFullscreen,
+}: {
+  onGameStatusChange: (status: string) => void;
+  onScreenshot: boolean;
+
+  onFullscreen: boolean;
+}) {
+  const [loadingPercentage, setLoadingPercentage] = useState<number>(0);
+  const [gameStatus, setGameStatus] = useState("");
+
   const {
     unityProvider,
     sendMessage,
     isLoaded,
     loadingProgression,
     requestFullscreen,
+    takeScreenshot,
   } = useUnityContext({
-    loaderUrl: "/Build/unity.loader.js",
-    dataUrl: "/Build/unity.data",
-    frameworkUrl: "/Build/unity.framework.js",
-    codeUrl: "/Build/unity.wasm",
+    loaderUrl: "build/level-creator/unity.loader.js",
+    dataUrl: "build/level-creator/unity.data",
+    frameworkUrl: "build/level-creator/unity.framework.js",
+    codeUrl: "build/level-creator/unity.wasm",
+    webglContextAttributes: {
+      preserveDrawingBuffer: true,
+    },
   });
+
+  useEffect(() => {
+    requestFullscreen(true);
+  }, [onFullscreen]);
+
+  useEffect(() => {
+    const dataUrl = takeScreenshot("image/jpg", 0.5);
+    //   newWindow.document.write('<img src="' + dataUrl + '" />');
+    console.log(dataUrl);
+  }, [onScreenshot]);
 
   useEffect(() => {
     setLoadingPercentage(Math.round(loadingProgression * 100));
@@ -31,44 +56,34 @@ export default function UnityLevelEmbed({ level }: { level?: Level }) {
   }, [isLoaded, loadingProgression]);
 
   function SendData() {
-    const combinedData = `//LEVEL_NAME:${level?.name}//,AUTHOR_NAME:${level?.author}//SEED:,${level?.seed}//`;
+    const combinedData = "true";
     console.log("TEST:  " + combinedData);
     sendMessage("GameManager", "FetchData", combinedData);
   }
 
+  const FetchData = useCallback((data: any) => {
+    setGameStatus(data);
+    onGameStatusChange(data);
+    console.log(data);
+  }, []);
+
   return (
-    <>
-      <div className="aspect-[16/9] relative">
+    <div className="w-full h-full flex justify-center items-center relative">
+      <>
         {isLoaded === false && (
-          <div className="absolute top-0 left-0 w-full h-full bg-gray-400 flex, justify-center items-center">
+          <div className="absolute top-0 left-0 w-full h-full bg-gray-400 flex flex-col justify-center items-center">
             <p className="text-6xl font-black text-center my-32">Loading...</p>
+
             <Progress
-              className="w-1/2 flex mx-auto"
+              className="w-1/2 mx-auto"
               value={loadingPercentage}
             ></Progress>
           </div>
         )}
-
-        <Unity unityProvider={unityProvider} className="w-full h-full" />
-      </div>
-      <div className="flex">
-        <div className="m-4">
-          <Button
-            onClick={() => {
-              requestFullscreen(true);
-            }}
-          >
-            Fullscreen
-          </Button>
+        <div className="aspect-[16/9] w-full h-full ">
+          <Unity unityProvider={unityProvider} className="w-full h-full" />
         </div>
-
-        <Button
-          className="m-4"
-          onClick={() => sendMessage("Testing", "TestingMethod", "Test")}
-        >
-          Click to Test
-        </Button>
-      </div>
-    </>
+      </>
+    </div>
   );
 }
