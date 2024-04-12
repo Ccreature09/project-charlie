663c066f5edb8313b63206bf53aa99d867f315ef
Ccreature09 <gabriellikov@gmail.com>
2024-04-13 00:08:07 +0300

    fix: custom email signin works

--- components/functional/signIn.tsx
+++ components/functional/signIn.tsx
@@ -5,6 +5,7 @@ import {
   GoogleAuthProvider,
 } from "firebase/auth";
 import { auth } from "@/firebase/firebase";
+import { useState } from "react";
 import {
   setDoc,
   serverTimestamp,
@@ -29,6 +30,8 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { ref, uploadBytes, getDownloadURL, getStorage } from "firebase/storage";
+const storage = getStorage();
 interface UserFormProps {
   login: boolean;
 }
@@ -37,6 +40,8 @@ const formSchema = z.object({
     message: "Invalid email format.",
   }),
   password: z.string().min(6),
+  username: z.string().min(3),
+  photo: z.instanceof(File).optional(),
 });
 const provider = new GoogleAuthProvider();
 
@@ -73,17 +78,26 @@ export const handleGoogleSignIn = async () => {
 };
 
 export default function UserForm({ login }: UserFormProps) {
+  const [step, setStep] = useState(1);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
+      username: "",
+      photo: undefined,
     },
   });
   async function onSubmit(values: z.infer<typeof formSchema>) {
     login
       ? handleSignIn(values.email, values.password)
-      : handleSignUp(values.email, values.password);
+      : handleSignUp(
+          values.email,
+          values.password,
+          values.username,
+          values.photo
+        );
   }
 
   const handleSignIn = async (email: string, password: string) => {
@@ -117,7 +131,12 @@ export default function UserForm({ login }: UserFormProps) {
     }
   };
 
-  const handleSignUp = async (email: string, password: string) => {
+  const handleSignUp = async (
+    email: string,
+    password: string,
+    username: string,
+    photo: File | undefined
+  ) => {
     try {
       const result = await createUserWithEmailAndPassword(
         auth,
@@ -125,6 +144,12 @@ export default function UserForm({ login }: UserFormProps) {
         password
       );
       const user = result.user;
+      let photoURL = "";
+      if (photo) {
+        const storageRef = ref(storage, `users/${user.uid}/profile.jpg`);
+        await uploadBytes(storageRef, photo);
+        photoURL = await getDownloadURL(storageRef);
+      }
 
       const userDocRef = doc(db, "users", user.uid);
       const querySnapshot = await query(
@@ -136,9 +161,9 @@ export default function UserForm({ login }: UserFormProps) {
       if (queryData.empty) {
         const userData = {
           uid: user.uid,
-          pfp: user.photoURL || "",
-          username: user.displayName || "User",
-          lowercaseUsername: user.displayName?.toLowerCase() || "user",
+          pfp: photoURL || "",
+          username: username,
+          lowercaseUsername: username.toLowerCase(),
           dateOfRegistration: serverTimestamp(),
           badges: [],
           levels: [],
@@ -167,45 +192,106 @@ export default function UserForm({ login }: UserFormProps) {
             </p>
 
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <div className="flex flex-row gap-3 ">
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem className="w-1/2">
-                      <FormLabel>Имейл</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="email@email.com"
-                          {...field}
-                          className="w-full p-2 rounded border"
-                        />
-                      </FormControl>
+              {step == 1 && (
+                <div className="flex flex-row gap-3 ">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem className="w-1/2">
+                        <FormLabel>Имейл</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="email@email.com"
+                            {...field}
+                            className="w-full p-2 rounded border"
+                          />
+                        </FormControl>
 
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem className="w-1/2">
-                      <FormLabel>Парола</FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder="*******"
-                          {...field}
-                          className="w-full p-2 rounded border"
-                          type="password"
-                        />
-                      </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem className="w-1/2">
+                        <FormLabel>Парола</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="*******"
+                            {...field}
+                            className="w-full p-2 rounded border"
+                            type="password"
+                          />
+                        </FormControl>
 
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              )}
+
+              {step == 2 && (
+                <div className="flex flex-row gap-3 ">
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem className="w-1/2">
+                        <FormLabel>Име</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Георги"
+                            {...field}
+                            className="w-full p-2 rounded border"
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+
+                  <FormField
+                    control={form.control}
+                    name="photo"
+                    render={({ field }) => (
+                      <FormItem className="w-1/2">
+                        <FormLabel>Снимка</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="file"
+                            accept="image/*"
+                            onChange={(e) =>
+                              e.target.files &&
+                              field.onChange(e.target.files[0])
+                            }
+                            onBlur={field.onBlur}
+                            className="w-full p-2 rounded border"
+                          />
+                        </FormControl>
+
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+              )}
+              {!login && (
+                <Button
+                  type="button"
+                  onClick={() => {
+                    step == 1 ? setStep(2) : setStep(1);
+                  }}
+                  className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+                >
+                  {step == 1 ? "Продължи" : " Назад"}
+                </Button>
+              )}
+
               <Button
                 type="submit"
                 className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
